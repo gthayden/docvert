37aab888fcfffddd31643108d85b77c9e6dc4cad
fmarier <fmarier@6e9fb932-9719-0410-8247-ed9ae3643143>
2007-11-07 11:08:35 +0000

    Properly set HOME and DISPLAY environment variables

--- core/config/unix-specific/convert-using-abiword.sh
+++ core/config/unix-specific/convert-using-abiword.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 inputDocumentUrl="$1"
+HOME=/var/www/docvert
 
 /usr/bin/abiword "${inputDocumentUrl}" --to=odt
--- core/config/unix-specific/convert-using-openoffice.org.sh
+++ core/config/unix-specific/convert-using-openoffice.org.sh
@@ -3,14 +3,14 @@ mockConversion="$1"
 docvertMacrosDocumentPath="$2"
 inputDocumentUrl="$3"
 outputDocumentUrl="$4"
+HOME=/var/www/docvert
 
 if [ $mockConversion = "true" ]
 then
-	XDisplayToUse="-display :0.0"
-	export localXPort=":0.0"
+	export DISPLAY=":0.0"
 else
 	xvfbRun="/usr/bin/xvfb-run"
 fi
 
 cd /tmp
-${xvfbRun} /usr/lib/openoffice/program/soffice -writer -norestore "${XDisplayToUse}" "${docvertMacrosDocumentPath}" macro://macros/Standard.convert.toOasisOpenDocumentFormat\(${inputDocumentUrl},${outputDocumentUrl}\)
+${xvfbRun} /usr/lib/openoffice/program/soffice -writer -norestore "${docvertMacrosDocumentPath}" macro://macros/Standard.convert.toOasisOpenDocumentFormat\(${inputDocumentUrl},${outputDocumentUrl}\)
