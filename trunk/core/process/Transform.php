97a7cbb2c09b24a7b1a308386f76e9bd97b97229
hollowaynz <hollowaynz@6e9fb932-9719-0410-8247-ed9ae3643143>
2007-11-19 11:21:38 +0000

    Abstracting language from transform process

--- trunk/core/process/Transform.php
+++ trunk/core/process/Transform.php
@@ -8,9 +8,9 @@ class Transform extends PipelineProcess
 	
 	public function process($currentXml)
 		{
-		if(!array_key_exists('withFile', $this->elementAttributes)) webServiceError('A pipeline\'s transform stage doesn\'t name an XSLT file. It should have a withFile attribute containing a filename relative to the pipeline directory.');
+		if(!array_key_exists('withFile', $this->elementAttributes)) webServiceError('&error-process-transform-lacks-withfile;');
 		$xslPath = $this->pipelineDirectory.$this->elementAttributes['withFile'];
-		if(!file_exists($xslPath)) webServiceError('A pipeline\'s transform stage refers to an XSL file that doesn\'t exist. There is no file (or I don\'t have permissions to read it) at: <tt>'.$xslPath.'</tt>');
+		if(!file_exists($xslPath)) webServiceError('&error-process-transform-withfile-missing-file;', 500, Array('xslPath'=>$xslPath));
 		$this->elementAttributes['loopdepth'] = $this->loopDepth;
 		return xsltTransform($currentXml, $xslPath, $this->elementAttributes);
 		}
