b541514150e05b176c7ea4da217bee37cf5e9cd2
hollowaynz <hollowaynz@6e9fb932-9719-0410-8247-ed9ae3643143>
2008-04-22 10:40:41 +0000

    Adding hashed passwords. Happy enough to force this change -- people can update their config files manually or delete it and start from scratch (their FTP/WebDAV settings won't be lost of course)

--- trunk/core/security.php
+++ trunk/core/security.php
@@ -4,14 +4,33 @@ include_once('config.php');
 
 class Security
 	{
+	static function hashPassword($password)
+		{
+		$salt = Security::getSaltValue();
+		return hash('sha256', $salt . $password);
+		}
+
 	static function setAdminPassword($password)
 		{
-		return setGlobalConfigItem('adminPassword', $password);
+		return setGlobalConfigItem('adminPassword', Security::hashPassword($password));
 		}
 
 	static function getAdminPassword()
 		{
 		return getGlobalConfigItem('adminPassword');
+		}
+
+	static function getSaltValue()
+		{
+		$configDirectory = getConfigDirectory();
+		$saltPath = $configDirectory.'salt';
+		if(file_exists($saltPath) && is_readable($saltPath))
+			{
+			return file_get_contents($saltPath);
+			}
+		$saltValue = hash('sha256', mt_rand() * microtime());
+		file_put_contents($saltPath, $saltValue);
+		return $saltValue;
 		}
 
 	}
