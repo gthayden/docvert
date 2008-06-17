6a4010054bf4140ba7abb8c625fe4bb48ca5a423
hollowaynz <hollowaynz@6e9fb932-9719-0410-8247-ed9ae3643143>
2008-06-17 04:00:48 +0000

    Abstracting FCKEditor configuration away from the library directory. Fixing error message for when preview directory is unwritable.

---
+++ trunk/core/themes/docvert/fckeditor-config.js
@@ -0,0 +1,5 @@
+// dynamically included by the FCKEditor
+
+FCKConfig.ToolbarSets["DocvertToolbar"] = [
+	['Style','-','Bold','Italic','-','Link','Unlink','OrderedList','UnorderedList','-','Table', '-','Source']
+] ;
