6926a717e52f71889e362b9a8bd26b31481854c7
hollowaynz <hollowaynz@6e9fb932-9719-0410-8247-ed9ae3643143>
2008-07-17 01:47:38 +0000

    Changing includes to reference by full paths (needs refactoring, testing)

--- core/config.php
+++ core/config.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once('lib.php');
+include_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'lib.php');
 
 function getConfigDirectory()
 	{
--- save-changes.php
+++ save-changes.php
@@ -4,8 +4,8 @@ $appDir = dirname(__file__).DIRECTORY_SEPARATOR;
 define('DOCVERT_DIR', $appDir);
 define('DOCVERT_CLIENT_TYPE', 'web');
 
-include_once('core/xslt.php');
-include_once('core/lib.php');
+include_once(DOCVERT_DIR.'core/xslt.php');
+include_once(DOCVERT_DIR.'core/lib.php');
 
 $unescapedPosts = array();
 
