b7ea222fa8e97d85d9fde185afcab8a06b6fcd97
hollowaynz <hollowaynz@6e9fb932-9719-0410-8247-ed9ae3643143>
2010-10-25 22:56:57 +0000

    Adding WMF/EMF documentation

--- core/config/unix-specific/convert-using-wmf2svg.sh
+++ core/config/unix-specific/convert-using-wmf2svg.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#NOTE: If wmf2gd isn't sufficient then read the docs in /doc/wmf-or-emf.txt
 inputImagePath="$1"
 outputImagePath="$2"
 
--- core/config/windows-specific/convert-using-wmf2svg.bat
+++ core/config/windows-specific/convert-using-wmf2svg.bat
@@ -1,1 +1,2 @@
+REM If wmf2gd isn't sufficient then read the docs in /doc/wmf-or-emf.txt
 @wmf2svg "%1" /o "%2"
